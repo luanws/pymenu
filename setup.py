f1f78f8f6e4c204f002413c16b8dd0e8ac7e9f7f
Luan Willig Silveira <luan.w.silveira@gmail.com>
2021-06-30 20:59:01 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pymenu",
+    name="pymenu-console",
     version="0.0.1",
     author="luanws",
     author_email="luan.w.silveira@gmail.com",
