64a7dcbe6cf53993ef2ff028533b29d9695ac67e
Luan Willig Silveira <luan.w.silveira@gmail.com>
2023-01-15 12:12:39 -0300

    fix(auto_menu): permite apagar números

--- pymenu/utils/inputs.py
+++ pymenu/utils/inputs.py
@@ -1,8 +1,8 @@
 import msvcrt
 
 
-def get_numeric_input():
-    input_str = ""
+def get_numeric_input() -> str:
+    input_str: str = ''
     while True:
         char = msvcrt.getch()
         if char.isdigit():
@@ -10,4 +10,9 @@ def get_numeric_input():
             print(char.decode(), end='', flush=True)
         elif char in b'\r\n':  # Enter key pressed
             break
+        elif char in b'\x08':  # Backspace key pressed
+            input_str = input_str[:-1]
+            print('\b \b', end='', flush=True)
+        elif char in b'\x03':  # Ctrl + C pressed
+            raise KeyboardInterrupt
     return input_str
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pymenu-console",
-    version="0.1.3",
+    version="0.1.4",
     author="luanws",
     author_email="luan.w.silveira@gmail.com",
     description="Python console menu",
